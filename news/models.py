c829b86173591c6f6f6f0f527ff9bb716910cc37
Thedywender <thedywender@hotmail.com>
2024-05-03 11:07:43 -0400

    req03 ok

--- news/models.py
+++ news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 
 class Category(models.Model):
@@ -16,3 +17,25 @@ class User(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+def validate_title(value):
+    words = value.split()
+    if len(words) <= 1:
+        raise ValidationError("O título deve ter mais de uma palavra.")
+
+
+class News(models.Model):
+    title = models.CharField(
+        max_length=200, validators=[validate_title], blank=False, null=False
+    )
+    content = models.TextField(blank=False, null=False)
+    author = models.ForeignKey("User", on_delete=models.CASCADE)
+    created_at = models.DateField(blank=False, null=False)
+    img = models.ImageField(upload_to="img/", blank=True, null=True)
+    categories = models.ManyToManyField(
+        Category, related_name="news", blank=False
+    )
+
+    def __str__(self) -> str:
+        return self.title

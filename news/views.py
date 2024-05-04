683b4415a7ce9904c4e9cffcb3ae2443537cb5fb
Thedywender <thedywender@hotmail.com>
2024-05-04 11:05:11 -0400

    req06 ok

--- news/views.py
+++ news/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from news.models import News
+from django.shortcuts import render, redirect
+from news.models import News, Category
+from news.forms import CategoryForm
 
 
 def home(request):
@@ -12,26 +13,12 @@ def news_details(request, id):
     return render(request, "news_details.html", {"news_details": context})
 
 
-# def categories_form(request):
-#     if request.method == "POST":
-#         category = CategoryForm(request.POST)
-#         if category.is_valid():
-#             category.save()
-#             request.session["category_id"] = category.instance.id
-#             return redirect("home-page")
-#     elif request.method == "GET":
-#         return render(
-#             request, "news_details.html", {"category_form": CategoryForm()}
-#         )
-
-
-# O template do formulário de uma nova categoria deve ter uma tag de formulário com a propriedade method do tipo post e a propriedade action com a url para /categories;
-# O template do formulário de uma nova categoria deve carregar o token de segurança CSRF em seu interior usando a tag de template adequada;
-# O template do formulário de uma nova categoria deve ter uma label que como o valor Nome;
-# O template do formulário de uma nova categoria deve ter um input com as algumas especificações:
-# A propriedade type do tipo text;
-# A propriedade name com o valor name;
-# A propriedade maxlength com o valor 200;
-# Precisa ser um campo obrigatório;
-# O template do formulário de uma nova categoria deve ter um botão do tipo submit com texto Salvar;
-# Após o cadastro de uma categoria, a pessoa usuária deve ser redirecionada para a página principal;
+def categories_form(request):
+    if request.method == "POST":
+        category = CategoryForm(request.POST)
+        if category.is_valid():
+            Category.objects.create(**category.cleaned_data)
+            return redirect("home-page")
+    else:
+        category = CategoryForm()
+    return render(request, "categories_form.html", {"form": category})
